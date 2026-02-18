1f85a85a18de122b32ce09197d603fb9cac5b475
Sebastian Hernandez <sebasht2003@gmail.com>
2026-02-18 11:11:04 -0500

    CRUD creation

--- src/project/base/urls.py
+++ src/project/base/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
-from . import  views
+from .views import PendingTasks, DetailedTask, CreateTask, UpdateTask, DeleteTask
 
-urlpatterns = [path('', views.to_do_list, name='request')]
+urlpatterns = [path('', PendingTasks.as_view(), name='pending'),
+               path('detailed/<int:pk>', DetailedTask.as_view(), name='detailed'),
+               path('create/', CreateTask.as_view(), name='create'),
+               path('update/<int:pk>', UpdateTask.as_view(), name='update'),
+               path('delete/<int:pk>', DeleteTask.as_view(), name='delete'),]
--- src/project/base/views.py
+++ src/project/base/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-def to_do_list(request):
-    return HttpResponse("Hello, world. This is the to do list.")
+from .models import Task
+
+class PendingTasks(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+class DetailedTask(DetailView):
+    model = Task
+    template_name = 'base/detailed_task.html'
+    context_object_name = 'task'
+
+class CreateTask(CreateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'base/task_form.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('pending')
+
+class UpdateTask(UpdateView):
+    model = Task
+    fields = '__all__'
+    template_name = 'base/task_update.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('pending')
+
+class DeleteTask(DeleteView):
+    model = Task
+    template_name = 'base/task_delete.html'
+    context_object_name = 'task'
+    success_url = reverse_lazy('pending')
